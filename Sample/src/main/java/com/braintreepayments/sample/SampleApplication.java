a8f121a8d1619dde0c85256808aea1a2713e795e
EarlSmartwave <37095419+EarlSmartwave@users.noreply.github.com>
2023-05-24 13:27:39 +0800

    Updated gradle setup (#9)
    Co-authored-by: EarlSmartwave <earl@smartwave.ph>

--- Sample/src/main/java/com/braintreepayments/sample/SampleApplication.java
+++ Sample/src/main/java/com/braintreepayments/sample/SampleApplication.java
@@ -10,7 +10,6 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
         Stetho.initializeWithDefaults(this);
     }
 }
